fe2dd31358e4c579178f796f353fd15609dab352
mbarajasespinoza <mbarajasespinoza@students.avc.edu>
2023-08-23 22:08:31 -0700

    test2

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ def main():
     total_perc = females + males
     f_perc = (females * 100) / total_perc
     m_perc = (males * 100) / total_perc
-    print ('Total percentage of females in class: ' + format(f_perc, '.2f') +'%')
-    print ('Total percentage of males in class: ' + format(m_perc, '.2f') +'%')
+    print('The total number of students: \t\t' + str(total_perc))
+    print ('Total percentage of females in class: \t' + format(f_perc, '.2f') +'%')
+    print ('Total percentage of males in class: \t' + format(m_perc, '.2f') +'%')
     return m_perc, f_perc
 
 
