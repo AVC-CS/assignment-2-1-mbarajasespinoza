7951c0e8593331b5aeeac0693d9759b06c01a659
mbarajasespinoza <mbarajasespinoza@students.avc.edu>
2023-08-23 22:11:38 -0700

    test2

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ def main():
     females = int(input('Enter the amount of females in class: '))
     males = int(input('Enter the amount of males in class: '))
     total_perc = females + males
-    f_perc = (females * 100) / total_perc
     m_perc = (males * 100) / total_perc
+    f_perc = (females * 100) / total_perc
     print('The total number of students: \t\t' + str(total_perc))
-    print ('Total percentage of females in class: \t' + format(f_perc, '.2f') +'%')
+    
     print ('Total percentage of males in class: \t' + format(m_perc, '.2f') +'%')
+    print ('Total percentage of females in class: \t' + format(f_perc, '.2f') +'%')
     return m_perc, f_perc
 
 
