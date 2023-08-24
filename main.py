3a49980fe9921dd7ca8d8b36fd6fee21d25c3819
mbarajasespinoza <mbarajasespinoza@students.avc.edu>
2023-08-23 21:55:54 -0700

    test1

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 def main():
-    """
-    ##################################################
-    Comlete your code here
-    Use m_perc and f_perc for your results
-    ##################################################
-    """
-
-    """
-    ########################################
-    # Do not delete the return statement
-    ########################################
-    """
+    females = int(input('Enter the amount of females in class: '))
+    males = int(input('Enter the amount of males in class: '))
+    total_perc = females + males
+    f_perc = (females * 100) / total_perc
+    m_perc = (males * 100) / total_perc
+    print ('Total percentage of females in class: ' + format(f_perc, '.2f') +'%')
+    print ('Total percentage of males in class: ' + format(m_perc, '.2f') +'%')
     return m_perc, f_perc
 
 
